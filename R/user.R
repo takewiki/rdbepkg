d6778e949067897ad17e2d273b558d8d983775a4
reshapedata <29032529@qq.com>
2023-12-09 04:21:00 +0000

    v0.3.3

--- R/user.R
+++ R/user.R
@@ -58,3 +58,63 @@ where FappId ='",FappId,"' and Fuser ='",Fuser,"'")
   tsda::sql_update2(token = token,sql_str = sql)
 
 }
+
+
+
+#' 查询用户的查看数据范围
+#'
+#' @param token 口令
+#' @param FappId 程序
+#' @param Fuser 用户
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' userDataRange_query()
+userDataRange_query <- function(token = '36F0DB19-AC55-4062-B2DA-39DC39B297BE',FappId ='jhdms',Fuser ='马斌') {
+
+  sql <- paste0("select  Fuser,Femail,Fphone,FBusinessMan,Fcompany,Fdepartment,FBusinessGroup,FCompanyScope,FDataScope  from rds_vw_md_userInfo
+where FappId ='",FappId,"' and Fuser ='",Fuser,"'")
+  userInfo = tsda::sql_select2(token = token,sql = sql)
+  ncount =nrow(userInfo)
+  if(ncount>0){
+    sql2 <- paste0("select  Fuser,Femail,Fphone,FBusinessMan,Fcompany,Fdepartment,FBusinessGroup,FCompanyScope,FDataScope from rds_vw_md_userInfo
+where FappId ='",FappId,"'")
+    if(userInfo$FCompanyScope == '所有公司'){
+      sql_Fcompany = " "
+
+    }else{
+      sql_Fcompany = paste0("  and  Fcompany ='",userInfo$Fcompany,"' ")
+    }
+
+    if(userInfo$FDataScope == '本人'){
+      sql_data = paste0(" and Fuser ='",Fuser,"'")
+    }
+
+    if(userInfo$FDataScope == '本业务组'){
+      sql_data = paste0(" and FBusinessGroup ='",userInfo$FBusinessGroup,"'")
+    }
+
+    if(userInfo$FDataScope == '本部门'){
+      sql_data = paste0(" and Fdepartment ='",userInfo$Fdepartment,"'")
+    }
+
+    if(userInfo$FDataScope == '所有部门'){
+      sql_data = paste0(" ")
+    }
+
+    sql_all = paste0(sql2,sql_Fcompany,sql_data)
+    print(sql_all)
+
+    res = tsda::sql_select2(token =token,sql = sql_all )
+
+  }else{
+    res = userInfo
+  }
+
+  return(res)
+
+
+
+}
